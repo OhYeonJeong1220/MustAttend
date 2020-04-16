3ae3a6c9ca2324288f191bcd9500d4fda726c7fc
seb1021 <inseb1021@gmail.com>
2020-04-16 14:15:26 +0900

    SBO Owner's Mypage+Owner's review management UI

---
+++ MayPage_Owner/app/src/main/java/com/example/mypage/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.mypage;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public void ReviewClicked(View v)
+    {
+        //SubActivity로 가는 인텐트를 생성
+        Intent intent = new Intent(this, ReviewManagment_Owner_Activity.class);
+        //액티비티 시작!
+        startActivity(intent);
+    }
+}
